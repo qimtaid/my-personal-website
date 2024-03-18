513dadd17371aa28bc3c38aba5deb38ced3e8174
Dan Koskei <qimtaid@gmail.com>
2024-03-18 15:57:28 +0300

    complete lab

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const button = document.getElementById("myButton");
+
+    /
+    button.addEventListener("click", function() {
+       
+        alert("Button clicked!");
+    });
+});
